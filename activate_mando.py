4aa28a939a33a9fff2b5cddc846ff43cceaf7b55
Mohab Mohamed Ali <62178212+mawhab@users.noreply.github.com>
2023-10-12 15:43:51 +0300

    Update activate_mando.py

--- activate_mando.py
+++ activate_mando.py
@@ -1,3 +1,4 @@
 import subprocess
 
-subprocess.Popen('!pip install -r /kaggle/working/ge-sc/requirements.txt -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html -f https://data.pyg.org/whl/torch-1.8.0+cu111.html -f https://data.dgl.ai/wheels/repo.html', shell=True)
+def install():
+  subprocess.Popen('!pip install -r /kaggle/working/ge-sc/requirements.txt -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html -f https://data.pyg.org/whl/torch-1.8.0+cu111.html -f https://data.dgl.ai/wheels/repo.html', shell=True)
